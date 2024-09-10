5016ca5b0b8884f387f2ce66137028f835af2de0
SALMA-Zrd <salmazraidi69@gmail.com>
2024-09-10 19:13:19 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+
+
+    var swiper = new Swiper(".slide_swip", {
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+      },
+      autoplay: {
+        delay: 2500,
+      },
+      loop: true,
+    });
+  
